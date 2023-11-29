7107dadc21132c566881f164b4dfca7b95ba0b5f
Daniel Aguiar <daniel.facull.udf@gmail.com>
2023-11-28 22:52:21 -0300

    Edited

--- zoologico/src/main/resources/auth-database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE `_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-)
