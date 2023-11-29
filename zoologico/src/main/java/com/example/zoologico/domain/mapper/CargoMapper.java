ae4d8a9c5ed9e4667835ad4c483e884342b64f5d
Daniel Aguiar <daniel.facull.udf@gmail.com>
2023-11-28 22:47:55 -0300

    Spring Security removed

--- zoologico/src/main/java/com/example/zoologico/domain/mapper/CargoMapper.java
+++ zoologico/src/main/java/com/example/zoologico/domain/mapper/CargoMapper.java
@@ -11,9 +11,9 @@ import com.example.zoologico.domain.model.Cargo;
 public interface CargoMapper {
   
   @Mapping(source = "cargo.id", target = "cargoCode")
-  @Mapping(source = "cargo.titulo", target = "cargoTitulo")
-  @Mapping(source = "cargo.descricao", target = "cargoDescricao")
-  @Mapping(source = "cargo.salarioBase", target = "salarioBase")
+  // @Mapping(source = "cargo.titulo", target = "cargoTitulo")
+  // @Mapping(source = "cargo.descricao", target = "cargoDescricao")
+  // @Mapping(source = "cargo.salarioBase", target = "salarioBase")
   public FuncionarioDTO toDto(Cargo cargo);
 
   @InheritInverseConfiguration
--- zoologico/src/main/java/com/example/zoologico/domain/mapper/DepartamentoMapper.java
+++ zoologico/src/main/java/com/example/zoologico/domain/mapper/DepartamentoMapper.java
@@ -11,8 +11,8 @@ import com.example.zoologico.domain.model.Departamento;
 public interface DepartamentoMapper {
   
   @Mapping(source = "departamento.id", target = "departamentoCode")
-  @Mapping(source = "departamento.nome", target = "departamentoNome")
-  @Mapping(source = "departamento.descricao", target = "departamentoDescricao")
+  // @Mapping(source = "departamento.nome", target = "departamentoNome")
+  // @Mapping(source = "departamento.descricao", target = "departamentoDescricao")
   public FuncionarioDTO toDto(Departamento departamento);
 
   @InheritInverseConfiguration
